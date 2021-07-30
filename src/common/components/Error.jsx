7356e25876df4f8634a1e5c12846f922091da84d
puneetsingh99 <99singhpuneet99@gmail.com>
2021-07-31 02:14:50 +0530

    feat: error page

---
+++ src/common/components/Error.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import errorImg from "../../assets/error_robo.png";
+
+export const Error = ({ message }) => {
+  return (
+    <article className="border w-full flex flex-col justify-center items-center">
+      <img src={errorImg} alt="error" className="sm:w-350 w-300" />
+      <div className="px-8 py-2 rounded-2xl sm:w-350 w-300 text-center">
+        <h1 className="text-2xl">{message}</h1>
+      </div>
+    </article>
+  );
+};
