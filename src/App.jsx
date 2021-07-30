63bcc397bbe50bf2b34a10a8755d47b301f195c0
puneetsingh99 <99singhpuneet99@gmail.com>
2021-07-30 16:10:44 +0530

    feat: configure tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-4xl text-red">Hi how are you?</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*.jsx"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
