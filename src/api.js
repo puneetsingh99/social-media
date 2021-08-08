171d92354662d9df03fe0597b7c0b9d37d90bb2d
puneetsingh99 <99singhpuneet99@gmail.com>
2021-08-08 19:02:53 +0530

    chore: configure API endpoint

--- src/api.js
+++ src/api.js
@@ -1,4 +1,6 @@
 export const API_ROOT = `https://protected-brook-51969.herokuapp.com`;
+const localhost = `http://localhost:5000`;
+
 export const API_ALL_POSTS = `${API_ROOT}/post`;
 export const API_ALL_USERS = `${API_ROOT}/user`;
 export const API_LOGIN = `${API_ROOT}/login`;
@@ -7,3 +9,5 @@ export const getUserAPI = (userId) => `${API_ROOT}/user/${userId}`;
 export const getPostsByUserAPI = (userId) => `${API_ROOT}/user/${userId}/posts`;
 export const updateFollowersAPI = (userId) =>
   `${API_ROOT}/user/${userId}/follow`;
+
+export const editProfileAPI = (userId) => `${API_ROOT}/user/${userId}`;
