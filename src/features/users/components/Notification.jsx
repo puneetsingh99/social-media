432cbabbf5d31401c1b4ea5b1f97e7d6103aba7f
puneetsingh99 <99singhpuneet99@gmail.com>
2021-08-11 11:57:44 +0530

    fix: notification click event

--- src/features/users/components/Notification.jsx
+++ src/features/users/components/Notification.jsx
@@ -1,31 +1,43 @@
 import React from "react";
 import { Avatar, TimeAgo } from "../../../common/components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Notification = ({ from, type, postId, createdAt }) => {
-  const { firstname, lastname, username, profilePic } = from;
+  const { firstname, lastname, username, profilePic, _id } = from;
+  const navigate = useNavigate();
   return (
-    <Link to={`/post/${postId}`} className="text-link">
-      <article className="flex px-2 py-3 border-b border-outline cursor-pointer hover:bg-dark-3-hover">
-        <aside className="min-w-max px-3">
+    <article
+      onClick={() => {
+        navigate(`/post/${postId}`);
+      }}
+      className="flex px-2 py-3 border-b border-outline cursor-pointer hover:bg-dark-3-hover"
+    >
+      <aside onClick={(e) => e.stopPropagation()} className="min-w-max px-3">
+        <Link to={`/user/${_id}`} className="text-link">
           <Avatar img={profilePic} size={"md"} />
-        </aside>
+        </Link>
+      </aside>
 
-        <div className="w-full">
-          <div className="text-md w-full flex items-start justify-between mb-2">
-            <div>
+      <div className="w-full">
+        <div className="text-md w-full flex items-start justify-between mb-2">
+          <div>
+            <Link
+              onClick={(e) => e.stopPropagation()}
+              to={`/user/${_id}`}
+              className="text-link"
+            >
               <h2 className="font-bold mr-1 hover:underline">{`${firstname} ${lastname}`}</h2>
-              <p className="text-text-gray mr-1">{`@${username}`}</p>
-            </div>
-            <div className="text-text-gray">
-              <TimeAgo timestamp={createdAt} />
-            </div>
+            </Link>
+            <p className="text-text-gray mr-1">{`@${username}`}</p>
           </div>
-          {type === "like" && <p className="text-lg">{`liked your post`}</p>}
-          {type === "comment" && <p>{`commented on your post`}</p>}
-          {type === "follow" && <p>{`started following you`}</p>}
+          <div className="text-text-gray">
+            <TimeAgo timestamp={createdAt} />
+          </div>
         </div>
-      </article>
-    </Link>
+        {type === "like" && <p className="text-lg">{`liked your post`}</p>}
+        {type === "comment" && <p>{`commented on your post`}</p>}
+        {type === "follow" && <p>{`started following you`}</p>}
+      </div>
+    </article>
   );
 };
