c55134b0f6ace0e62eea24a9a6825785c046aaa4
puneetsingh99 <99singhpuneet99@gmail.com>
2021-08-26 20:26:02 +0530

    refactor: replace switch with enum at Avatar.jsx
    change switch to enum in Avatar.jsx to increase readability

--- src/common/components/Avatar.jsx
+++ src/common/components/Avatar.jsx
@@ -1,37 +1,17 @@
 import React from "react";
 import defaultPic from "../../assets/default_profile.png";
 
+const avatarSize = {
+  xs: "w-6 h-6",
+  sm: "w-8 h-8",
+  md: "w-12 h-12",
+  lg: "w-16 h-16",
+  xl: "w-20 h-20",
+  "2xl": "w-32 h-32",
+};
+
 export const Avatar = ({ img, size, hover }) => {
-  let avatarDimensions;
-
-  switch (size) {
-    case "xs":
-      avatarDimensions = "w-6 h-6";
-      break;
-    case "sm":
-      avatarDimensions = "w-8 h-8";
-      break;
-
-    case "md":
-      avatarDimensions = "w-12 h-12";
-      break;
-
-    case "lg":
-      avatarDimensions = "w-16 h-16";
-      break;
-
-    case "xl":
-      avatarDimensions = "w-20 h-20";
-      break;
-
-    case "2xl":
-      avatarDimensions = "w-32 h-32";
-      break;
-
-    default:
-      avatarDimensions = "w-12 h-12";
-      break;
-  }
+  let avatarDimensions = avatarSize[size] || "w-12 h-12";
 
   const imgSrc = img || defaultPic;
 
