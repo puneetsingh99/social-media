4b459e715325d1914646ddac85022ca7f6e0cd2d
puneetsingh99 <99singhpuneet99@gmail.com>
2021-08-26 19:48:06 +0530

    fix: support fullname search

--- src/common/components/side-bar/search.js
+++ src/common/components/side-bar/search.js
@@ -4,11 +4,13 @@ export const search = (keyword, user) => {
   const username = lowerCase(user.username);
   const firstname = lowerCase(user.firstname);
   const lastname = lowerCase(user.lastname);
+  const fullname = lowerCase(`${firstname} ${lastname}`);
   const lCaseKeyword = lowerCase(keyword);
 
   const checkOne = username.search(lCaseKeyword);
   const checkTwo = firstname.search(lCaseKeyword);
   const checkThree = lastname.search(lCaseKeyword);
+  const checkFour = fullname.search(lCaseKeyword);
 
   if (checkOne !== -1) {
     return true;
@@ -19,6 +21,10 @@ export const search = (keyword, user) => {
   }
 
   if (checkThree !== -1) {
+    return true;
+  }
+
+  if (checkFour !== -1) {
     return true;
   }
 
