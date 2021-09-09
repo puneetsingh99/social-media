2133c3dbbbacc1af121557ba1fc4588007f13e32
puneetsingh99 <99singhpuneet99@gmail.com>
2021-09-10 02:25:08 +0530

    chore: change server

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
-export const API_ROOT = `https://protected-brook-51969.herokuapp.com`;
+// export const API_ROOT = `https://protected-brook-51969.herokuapp.com`;
 // export const API_ROOT = `http://localhost:5000`;
+export const API_ROOT = `https://social-media-be.puneetsingh2.repl.co`;
 
 export const API_ALL_POSTS = `${API_ROOT}/post`;
 
